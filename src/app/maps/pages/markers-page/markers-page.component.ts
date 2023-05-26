907eb16231adf01e5c7c8ffce84f416c0a70d186
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-25 21:33:07 -0500

    marcadores en el mapa

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,8 +1,40 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 @Component({
   selector: 'maps-markers-page',
   templateUrl: './markers-page.component.html',
   styleUrls: ['./markers-page.component.css'],
 })
-export class MarkersPageComponent {}
+export class MarkersPageComponent implements AfterViewInit {
+  @ViewChild('map')
+  public divMap?: ElementRef;
+
+  public zoom: number = 13;
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(
+    -76.51681149679513,
+    3.4615345016951835
+  );
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw 'El elemento html no fue encontrado';
+
+    this.map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: this.zoom, // starting zoom
+    });
+
+    //* como personalizar el marcador del mapa
+    // const markerHtml = document.createElement('div');
+    // markerHtml.innerHTML = 'Kevin Echeverri';
+
+    // const marker = new Marker({
+    //   color: 'green',
+    //   element: markerHtml,
+    // })
+    //   .setLngLat(this.currentLngLat)
+    //   .addTo(this.map);
+  }
+}
