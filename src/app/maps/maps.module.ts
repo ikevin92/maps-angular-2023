04988ab9e579f5992de13c627411fd3d6b4eb521
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-26 09:43:52 -0500

    standalone como pagina

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -3,8 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import * as mapboxgl from 'mapbox-gl';
 
-(mapboxgl as any).accessToken =
-  'pk.eyJ1IjoiaWtldmluOTIiLCJhIjoiY2tpaWQ3NzBlMDhoODJ3bnhjeDdnNDE1MiJ9.3bNHLj9E8XyrANMYSXzn8Q';
+// (mapboxgl as any).accessToken =
+//   'pk.eyJ1IjoiaWtldmluOTIiLCJhIjoiY2tpaWQ3NzBlMDhoODJ3bnhjeDdnNDE1MiJ9.3bNHLj9E8XyrANMYSXzn8Q';
+(mapboxgl as any).accessToken = environment.mapbox_key;
 
 import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
@@ -14,6 +15,7 @@ import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-pa
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
+import { environment } from 'src/environments/environments';
 
 @NgModule({
   declarations: [
