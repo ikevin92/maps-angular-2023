cc1023533109b7aba21c7fffa9370f25e5d92f2c
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-26 00:56:11 -0500

    flyTo

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -77,4 +77,11 @@ export class MarkersPageComponent implements AfterViewInit {
     this.markers[index].marker.remove();
     this.markers.splice(index, 1);
   }
+
+  flyTo(marker: Marker) {
+    this.map?.flyTo({
+      zoom: 14,
+      center: marker.getLngLat(),
+    });
+  }
 }
